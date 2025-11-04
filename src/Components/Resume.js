7049fda44c9fc8c5d7ee7696e9d936acc652a4f2
Lumintitus <sekarlumin@gmail.com>
2025-11-04 16:53:48 +0530

    Add resume PDF download link and fix import typo

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -1,4 +1,6 @@
 import resume from "../images/Resume-pana.png"
+import documet from "../images/Lumin resume.pdf"
+
 export default function Resume() {
     return (
         <section id='resume' className="flex flex-col md:flex-row bg-secondary text-white font-hero-font px-6">
@@ -8,7 +10,7 @@ export default function Resume() {
             <div className="md:w-1/2 flex justify-center">
                 <div className="flex flex-col justify-center">
                     <h1 className="text-3xl border-b-4 mb-5 font-bold w-[7rem]">Resume</h1>
-                    <p className="text-xl">You can view my resume <a className="btn" href="#">Download</a></p>
+                    <p className="text-xl">You can view my resume <a className="btn" href={documet} download>Download</a></p>
                 </div>
 
             </div>
